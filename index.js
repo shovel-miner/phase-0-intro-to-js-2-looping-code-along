354a1b3476f06eaa4e257f82cc6639fefa466ca2
shovel-miner <90447494+shovel-miner@users.noreply.github.com>
2022-01-27 10:30:01 -0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, event){
+    let Thankyou =[]
+    for (let i = 0; i = names.length; i++) {
+        Thankyou.push('Thank you, ${names[i]}, for the wonderful ${event} gifts!');
+    debugger;
+    }
+    return Thankyou;
+}
+writeCards(names)
+
+ function countDown(num){
+    while (num >=0){;
+        console.log(num);
+        num--;
+        }
+}
